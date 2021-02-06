08d8158bff8dc78a77335788b46d9c3a6f199c83
Емельянов Александр <a.yemelyanov@gmail.com>
2021-02-06 22:56:00 +0600

    Initial commit

---
+++ lib/json_fetcher.dart
@@ -0,0 +1,249 @@
+// Copyright (c) 2020, alex@justprodev.com.
+// All rights reserved. Use of this source code is governed by a
+// MIT License that can be found in the LICENSE file.
+
+/// Tools to easily work with JSON services / data over HTTP.
+/// 1. http client wrapper [HttpClient]
+/// 2. json fetcher [HttpJsonFetcher]
+/// 3. cache [JsonCacheManager]
+
+import 'package:meta/meta.dart';
+import 'dart:async';
+import 'dart:collection';
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:http/http.dart' as http;
+import 'package:logging/logging.dart';
+
+///
+/// Universal fetcher for JSON by HTTP
+///
+abstract class HttpJsonFetcher<T> {
+  int _step = 0;
+  /// implement me: returns object parsed from json
+  Future<T> parse(String source);
+
+  /// set [allowErrorWhenCacheExists] to true if you want to see network errors always
+  /// by default, if cached version is available then errors will not be pushed to stream
+  Stream<T> fetch(String url, {allowErrorWhenCacheExists: false, nocache: false}) => HttpClient._fetch(url, allowErrorWhenCacheExists: allowErrorWhenCacheExists, nocache: nocache).asyncMap((s)=>_parse(url, s));
+
+  Future<T> _parse(String url, String source) async {
+    try {
+      final o = await parse(source);
+      if(_step>0 && HttpClient._fetchHandler!=null) HttpClient._fetchHandler(url, o);
+      _step++;
+      return o;
+    } catch(e, trace) {
+      if(_step == 0) return null; // from cache
+
+      throw e;
+    }
+  }
+}
+
+///
+/// Universal fetcher for JSON by HTTP (no cache)
+///
+abstract class HttpJsonGetter<T> {
+  /// implement me: returns object parsed from json
+  Future<T> parse(String source);
+
+  Future<T> get(String url) => HttpClient.get(url).then((s)=>parse(s.body));
+}
+
+class HttpClient {
+  static http.Client _client = http.Client();
+  static final _log = Logger((HttpClient).toString());
+  /// permanent headers across client
+  static Map<String,String> _headers = Map();
+  static Function() _onBearerExpire;
+  static Function(String url, dynamic document) _fetchHandler;
+
+  /// Let's set the [bearer] that was obtained anywhere
+  /// "Authorization: Bearer [bearer]" will be added permanently to headers
+  /// [onBearerExpire] will be called for 401 error (bearer will be removed from the header immediately)
+  static void setAuth(String bearer, Function onBearerExpire) {
+    _headers["Authorization"] = "Bearer " + bearer;
+    _onBearerExpire = () {
+      _headers.remove("Authorization");
+      onBearerExpire();
+    };
+  }
+
+  /// [handler] will be called when [HttpJsonFetcher] completes parsing an document
+  /// which was fetched from [url] from network (not from cache!)
+  /// Please be sure, that handler works fast as possible (do heavy operations async)
+  static void setFetchHandler(Function(String url, dynamic document) handler) {
+    _fetchHandler = handler;
+  }
+
+  static Stream<String> _fetch(String url, {allowErrorWhenCacheExists = false, nocache = false}) {
+    var controller = StreamController<String>();
+    bool hasData = false;
+    JsonCacheManager().readAsString(url, headers: _headers, nocache: nocache).listen((String s) { controller.add(s); hasData = true; },
+        onError: (e) {
+          /// for 401 error we silently invoke onExpire handler
+          if (e is HttpException && e.message.contains('401') &&
+              _onBearerExpire != null) _onBearerExpire();
+          if(allowErrorWhenCacheExists || !hasData) controller.addError(e);
+          _log.severe("Error while fetching $url", e);
+        }, onDone: () => controller.close());
+
+    return controller.stream;
+  }
+
+  static Future<http.Response> post(String url, String json, {Map<String,String> headers}) async {
+    Map<String, String> h = {"Content-Type": "application/json"};
+    h.addAll(_headers);
+    if(headers!=null) h.addAll(headers);
+    var response = await _client.post(url, body: json, headers: h);
+    final String contentType = response.headers[HttpHeaders.contentTypeHeader] ?? "application/json";
+    if(!contentType.contains("charset")) response.headers[HttpHeaders.contentTypeHeader] = contentType + ";charset=utf-8";
+    if(response.statusCode == 401) { if(_onBearerExpire!=null) _onBearerExpire(); throw "$url: Пользователь не авторизован"; }
+    return response;
+  }
+
+  static Future<http.Response> put(String url, String json, {Map<String,String> headers}) async {
+    Map<String, String> h = {"Content-Type": "application/json"};
+    h.addAll(_headers);
+    if(headers!=null) h.addAll(headers);
+    var response = await _client.put(url, body: json, headers: h);
+    final String contentType = response.headers[HttpHeaders.contentTypeHeader] ?? "application/json";
+    if(!contentType.contains("charset")) response.headers[HttpHeaders.contentTypeHeader] = contentType + ";charset=utf-8";
+    if(response.statusCode == 401) { if(_onBearerExpire!=null) _onBearerExpire(); throw "$url: Пользователь не авторизован"; }
+    return response;
+  }
+
+  /// get request without caching (unlike [_fetch])
+  static Future<http.Response> get(String url) async {
+    Map<String, String> h = {"Content-Type": "application/json"};
+    h.addAll(_headers);
+    var response = await _client.get(url, headers: h);
+    final String contentType = response.headers[HttpHeaders.contentTypeHeader] ?? "application/json";
+    if(!contentType.contains("charset")) response.headers[HttpHeaders.contentTypeHeader] = contentType + ";charset=utf-8";
+    if (response.statusCode < 200 || response.statusCode >= 400) {
+      if(response.statusCode == 401) { if(_onBearerExpire!=null) _onBearerExpire(); throw "$url: Пользователь не авторизован"; }
+      throw HttpClientException("", response);
+    }
+    return response;
+  }
+
+  static Future<http.Response> delete(String url) async {
+    var response = await _client.delete(url, headers: _headers);
+    if (response.statusCode < 200 || response.statusCode >= 400) {
+      if(response.statusCode == 401) { if(_onBearerExpire!=null) _onBearerExpire(); throw "$url: Пользователь не авторизован"; }
+      throw HttpClientException("", response);
+    }
+    return response;
+  }
+
+  static void logout() => _onBearerExpire();
+
+  @visibleForTesting
+  static void setClient(http.Client client) { _client = client; }
+}
+
+class HttpClientException implements HttpException {
+  final http.Response response;
+  final String message;
+
+  HttpClientException(this.message, this.response);
+
+  String toString() {
+    return message + " (" + response.statusCode.toString() +")";
+  }
+
+  @override
+  Uri get uri => response.request.url;
+}
+
+/// [BaseCacheManager] variant that always download file (after returning it from memory or cache firstly)
+class JsonCacheManager {
+  static final String _key = (JsonCacheManager).toString();
+  static JsonCacheManager _instance;
+
+  Map<String, StreamController<String>> _downloads = HashMap();
+  final _log = Logger(_key);
+  CacheManager _cache;
+
+  factory JsonCacheManager() {
+    if (_instance == null) {
+      final cache = CacheManager(
+        Config(
+          _key,
+          stalePeriod: const Duration(days: 30),
+          maxNrOfCacheObjects: 100,
+          fileService: HttpFileService(httpClient: HttpClient._client),
+        ),
+      );
+      _instance = JsonCacheManager._internal(cache);
+    }
+    return _instance;
+  }
+
+  JsonCacheManager._internal(this._cache);
+
+  /// [nocache] skips cache before getting the file - i.e.get from Internet then cache it
+  Stream<String> readAsString(String url, {Map<String, String> headers, nocache: false}) {
+    StreamController<String> controller = _downloads[url];
+
+    if(controller!=null && !controller.isClosed) controller.close(); // prev download started, drop it
+
+    controller = StreamController();
+    _downloads[url] = controller;
+
+    void _getFile() async {
+      try {
+        FileInfo f;
+        String cachedString;
+
+        if(!nocache) {
+          f = await _cache.getFileFromCache(url);
+          if (f != null) if (!controller.isClosed) controller.add(
+              cachedString = await f.file.readAsString()); // cache
+        }
+
+        //print("download $url start");
+        var webFile = await _cache.downloadFile(url, authHeaders: headers, force: true);
+        //print("download $url stop");
+        if (webFile != null && !controller.isClosed) {
+          final String onlineString = await webFile.file.readAsString();
+          if(onlineString != cachedString) // skip if a data the same
+            controller.add(onlineString);
+        } // online
+      } catch (e, trace) {
+        _log.severe("Failed to download file from $url", e, trace);
+        if(!controller.isClosed) controller.addError(e);
+      } finally {
+        if(!controller.isClosed) await controller.close();
+        _downloads.remove(url);
+      }
+    }
+
+    _getFile();
+
+    return controller.stream;
+  }
+
+  Future<void> removeFile(key) => _cache.removeFile(key);
+  Future<void> emptyCache() => _cache.emptyCache();
+}
+
+extension Decoder on http.Response {
+  /// decode body to Map && check any possibly error returned by backend
+  Future<Map<String, dynamic>> decode({errorPrefix = "Ошибка сервера"}) async {
+    final response = this;
+    final json = await compute(_decode, body);
+    if (response.statusCode < 200 || response.statusCode >= 400
+        || json["status"] == 400 || json["errorMessage"] != null) {
+      throw HttpClientException(errorPrefix, response);
+    }
+    return json;
+  }
+
+  static Map<String, dynamic> _decode(String jsonData) {
+    return json.decode(jsonData);
+  }
+}
---
+++ test/json_fetcher_test.dart
@@ -0,0 +1,93 @@
+// Copyright (c) 2020, alex@justprodev.com.
+// All rights reserved. Use of this source code is governed by a
+// MIT License that can be found in the LICENSE file.
+
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:json_fetcher/json_fetcher.dart';
+import 'package:mock_web_server/mock_web_server.dart';
+
+final GET_TYPICALS_METHOD = "gettypicals"+DateTime.now().millisecond.toString();
+const TYPICAL_DATA1 = "data1";
+const TYPICAL_DATA2 = "data2";
+
+class Typical {
+  String data;
+
+  static Typical fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    Typical typical = Typical();
+    typical.data = map['data'];
+    return typical;
+  }
+
+  // needed just for test
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Typical &&
+          runtimeType == other.runtimeType &&
+          data == other.data;
+
+  // needed just for test
+  @override
+  int get hashCode => data.hashCode;
+}
+
+class _TypicalFetcher extends HttpJsonFetcher<List<Typical>> {
+  /// compute json parsing in separated thread
+  @override
+  Future<List<Typical>> parse(String source) => compute(_parseTypicals, source);
+
+  static List<Typical> _parseTypicals(String source) {
+    final parsed = json.decode(source);
+    return parsed.map<Typical>((json) => Typical.fromMap(json)).toList();
+  }
+}
+
+Stream<List<Typical>> fetchTypicals(String prefix) => _TypicalFetcher().fetch(prefix+GET_TYPICALS_METHOD);
+
+void main() {
+  test('integration test of the cache (client + parser + cache)',() async {
+    MockWebServer server = new MockWebServer(port: 8082);
+    await server.start();
+
+    final String prefix = server.url;
+
+    // The response queue is First In First Out
+    // 1 not cached
+    server.enqueue(body: '[{ "data": "$TYPICAL_DATA1"}]');
+    // 1 cached + 1 not cached
+    server.enqueue(body: '[{ "data": "$TYPICAL_DATA1"}, { "data": "$TYPICAL_DATA2"}]');
+    // 1 cached + 1 cached
+    server.enqueue(body: '[{ "data": "$TYPICAL_DATA1"}, { "data": "$TYPICAL_DATA2"}]');
+
+    List<Typical> generate(List<String> data) => List<Typical>()..addAll(data.map((e) => Typical()..data = e));
+
+    var s = fetchTypicals(prefix).listen((event) {
+      expect(event, equals(generate([TYPICAL_DATA1])));
+    });
+    await s.asFuture();s.cancel();
+
+    var count = 1;
+    s = fetchTypicals(prefix).listen((event) {
+      if(count == 1) expect(event, equals(generate([TYPICAL_DATA1])));  // 1 cached
+      else if(count == 2) expect(
+          event, equals(generate([TYPICAL_DATA1, TYPICAL_DATA2])));    // 1 cached + 1 not cached
+      count++;
+    });
+    await s.asFuture();s.cancel();
+
+    count = 0;
+    s = fetchTypicals(prefix).listen((event) {
+      expect(event, equals(generate([TYPICAL_DATA1, TYPICAL_DATA2])));    // 1 cached + 1 cached
+      count++;
+    });
+    await s.asFuture();s.cancel();
+    // stream should produce only one event - list from the cache
+    // and list from online should be skipped because it's equals to cache
+    expect(count, equals(1));
+  });
+}
