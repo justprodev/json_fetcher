fd104ea43045da4905b050c3ac5494809633d8b1
Alexander Yemelyanov <alex@justprodev.com>
2022-09-30 01:45:48 +0600

    ability to configure the logging

--- lib/loggable_http_client.dart
+++ lib/loggable_http_client.dart
@@ -15,9 +15,9 @@ const normalJsonFieldLength = 1000; // 1kB
 class LoggableHttpClient extends BaseClient {
   final Client _delegate;
   final Logger _logger;
-  final bool cutLongBody;
+  final LoggableHttpClientConfig config;
 
-  LoggableHttpClient(this._delegate, this._logger, {this.cutLongBody = false});
+  LoggableHttpClient(this._delegate, this._logger, {this.config = const LoggableHttpClientConfig()});
 
   @override
   void close() {
@@ -27,19 +27,22 @@ class LoggableHttpClient extends BaseClient {
   @override
   Future<StreamedResponse> send(BaseRequest request) async {
     String s = "${request.method} ${request.url} -->";
-    s += "\nheader: ${request.headers}";
-    if (request is Request && request.body.length > 0) {
+    s += "\nheader: ${config.hideAuthorization ? _hideAuthorization(request.headers) : request.headers}";
+    if (config.logInputBody && request is Request && request.body.length > 0) {
       s += "\nbody: ${await _smartCut(request.headers['Content-Type'], request.body)}";
     }
     _logger.info(s);
+
     final response = await _delegate.send(request);
     s = "${request.method} ${request.url} <-- ${response.statusCode}";
-    s += "\nheader: ${response.headers}";
+    s += "\nheader: ${config.hideAuthorization ? _hideAuthorization(response.headers) : response.headers}";
 
     // Simple request
     if (request is Request) {
       final List<int> bytes = await response.stream.toBytes();
-      s += "\nbody: ${await _smartCut(response.headers['Content-Type'] ?? response.headers['content-type'], utf8.decode(bytes))}";
+      if (config.logOutputBody)
+        s +=
+            "\nbody: ${await _smartCut(response.headers['Content-Type'] ?? response.headers['content-type'], utf8.decode(bytes))}";
       _logger.info(s);
 
       return StreamedResponse(ByteStream.fromBytes(bytes), response.statusCode,
@@ -56,9 +59,14 @@ class LoggableHttpClient extends BaseClient {
     return response;
   }
 
-  // cut a body if necessary (long)
+  Map<String, String> _hideAuthorization(Map<String, String> headers) {
+    final entries = headers.entries.map((e) => e.key.toLowerCase() == 'authorization' ? MapEntry(e.key, '...') : e);
+    return Map.fromEntries(entries);
+  }
+
+  // cut a body if necessary (cut a long json values or full body)
   Future<String> _smartCut(String? contentType, String body) async {
-    if (cutLongBody && body.length > longBodyLength) {
+    if (config.cutLongBody && body.length > longBodyLength) {
       if (contentType?.contains('/json') == true) {
         return compute(_smartCutJson, body);
       } else {
@@ -84,4 +92,20 @@ class LoggableHttpClient extends BaseClient {
       },
     );
   }
+}
+
+/// Configure "What to log" in [LoggableHttpClient]
+/// input - is look from server side
+class LoggableHttpClientConfig {
+  final bool logInputBody;
+  final bool logOutputBody;
+  final bool hideAuthorization;
+  final bool cutLongBody;
+
+  const LoggableHttpClientConfig({
+    this.logInputBody = true,
+    this.logOutputBody = false,
+    this.hideAuthorization = true,
+    this.cutLongBody = true,
+  });
 }
--- test/json_fetcher_logger_test.dart
+++ test/json_fetcher_logger_test.dart
@@ -19,25 +19,27 @@ import 'fake_path_provider.dart';
 void main() {
   setUpFakePathProvider();
 
-  final Map<String,String> authHeaders1 = {'authorization': 'Bearer 12345'};
-  final Map<String,String> authHeaders2 = {'authorization': 'Bearer 67890'};
+  final Map<String, String> authHeaders1 = {'authorization': 'Bearer 12345'};
+  final Map<String, String> authHeaders2 = {'authorization': 'Bearer 67890'};
 
   final r = Random();
   String bigString() => List.generate(longJsonFieldLength, (index) => r.nextBool() ? 'A' : 'B').join('');
-  List<String> bigArray() => List.generate(longBodyLength~/longJsonFieldLength, (index) => '"${bigString()}"');
+  List<String> bigArray() => List.generate(longBodyLength ~/ longJsonFieldLength, (index) => '"${bigString()}"');
 
-  JsonHttpClient createClient({cutLongBody = false}) {
+  JsonHttpClient createClient({config = const LoggableHttpClientConfig()}) {
     var selectedAuthHeaders = authHeaders1;
-    final JsonHttpClient client = JsonHttpClient(
-        LoggableHttpClient(Client(), Logger((JsonHttpClient).toString()), cutLongBody: cutLongBody),
-        auth: AuthInfo(() => selectedAuthHeaders, (bool) async { selectedAuthHeaders = authHeaders2; return true; })
-    );
+    final JsonHttpClient client =
+        JsonHttpClient(LoggableHttpClient(Client(), Logger((JsonHttpClient).toString()), config: config),
+            auth: AuthInfo(() => selectedAuthHeaders, (bool) async {
+              selectedAuthHeaders = authHeaders2;
+              return true;
+            }));
     client.cache.emptyCache();
     return client;
   }
 
-  test('logging big json', () async {
-    final JsonHttpClient client = createClient(cutLongBody: true);
+  test('cut big json body', () async {
+    final JsonHttpClient client = createClient(config: LoggableHttpClientConfig(cutLongBody: true));
     final MockWebServer server = new MockWebServer(port: 8082);
     await server.start();
 
@@ -47,16 +49,16 @@ void main() {
 
     // post big json
     server.enqueue(body: 'test response body');
-    Logger.root.onRecord.listen((record) {
+    final subs = Logger.root.onRecord.listen((record) {
       int i = record.message.indexOf('{ "data');
-      if(i > 0) {
+      if (i > 0) {
         final Map big = json.decode(bigJson);
         final Map cut = json.decode(record.message.substring(i));
 
         expect(cut['data'].length == big['data'].length, true);
 
         var j = 0;
-        for(final String fCut in cut['data']) {
+        for (final String fCut in cut['data']) {
           final fBig = big['data'][j++];
           final subs = fCut.split('...');
           expect(fBig.startsWith(subs[0]), true);
@@ -65,13 +67,14 @@ void main() {
         }
       }
     });
-    await client.post(prefix+'a', bigJson, headers: {'Content-Type': 'application/json'});
+    await client.post(prefix + 'a', bigJson, headers: {'Content-Type': 'application/json'});
 
     await server.shutdown();
+    await subs.cancel();
   });
 
-  test('logging plain', () async {
-    final JsonHttpClient client = createClient(cutLongBody: true);
+  test('cut big plain body', () async {
+    final JsonHttpClient client = createClient(config: LoggableHttpClientConfig(cutLongBody: true));
     final MockWebServer server = new MockWebServer(port: 8082);
     await server.start();
 
@@ -81,9 +84,9 @@ void main() {
 
     // post big plain
     server.enqueue(body: bigPlain);
-    Logger.root.onRecord.listen((record) {
+    final subs = Logger.root.onRecord.listen((record) {
       final ss = record.message.split('body: ');
-      if(ss.length > 2) {
+      if (ss.length > 1) {
         final String cut = ss[1];
 
         final subs = cut.split('...');
@@ -91,8 +94,101 @@ void main() {
         expect(bigPlain.length > cut.length, true);
       }
     });
-    await client.get(prefix+'a');
+    await client.get(prefix + 'a');
 
     await server.shutdown();
+    await subs.cancel();
+  });
+
+  Future<void> testAuth(bool show) async {
+    final JsonHttpClient client = createClient(config: LoggableHttpClientConfig(hideAuthorization: !show));
+    final MockWebServer server = new MockWebServer(port: 8082);
+    await server.start();
+    final String prefix = server.url;
+
+    server.enqueue(body: 'body', headers: {'Content-type': 'application/json', ...authHeaders1});
+    final subs = Logger.root.onRecord.listen((record) {
+      if (record.message.contains('<--')) return; // skip incoming
+      debugPrint(record.toString());
+      final ss = record.message.split('header: ');
+      assert(ss.length == 2, 'headers isn\'t present');
+      if (ss.length > 1) {
+        final Map<String, String> headers = Map.fromEntries(
+          ss[1].substring(1, ss[1].length - 1).split(', ').map(
+            (e) {
+              final entry = e.split(': ');
+              return MapEntry(entry[0].toLowerCase(), entry[1]);
+            },
+          ),
+        );
+        assert(headers['content-type'] == 'application/json', 'Content-type is broken');
+        if (show) {
+          assert(headers['authorization'] == authHeaders1.values.first, 'Authorization did not shown');
+        } else {
+          assert(headers['authorization'] != authHeaders1.values.first, 'Authorization did not hidden');
+        }
+      }
+    });
+    await client.get(prefix + 'a');
+
+    await server.shutdown();
+    await subs.cancel();
+  }
+
+  test('hide auth', () async {
+    await testAuth(false);
+  });
+
+  test('show auth', () async {
+    await testAuth(true);
+  });
+
+  Future<void> testBody(bool input, bool show) async {
+    final JsonHttpClient client;
+    if(input) {
+      client = createClient(config: LoggableHttpClientConfig(logInputBody: show));
+    } else {
+      client = createClient(config: LoggableHttpClientConfig(logOutputBody: show));
+    }
+    final MockWebServer server = new MockWebServer(port: 8082);
+    await server.start();
+    final String prefix = server.url;
+
+    server.enqueue(body: 'body', headers: {'Content-type': 'application/json', ...authHeaders1});
+    final subs = Logger.root.onRecord.listen((record) {
+      if (input) {
+        if (record.message.contains('<--')) return; // skip incoming
+      } else {
+        if (record.message.contains('-->')) return; // skip outgoing
+      }
+
+      debugPrint(record.toString());
+      if(show) {
+        assert(record.message.indexOf('body: body') > -1, 'body isn\'t present');
+      } else {
+        assert(record.message.indexOf('body: body') == -1, 'body is present');
+      }
+
+    });
+    await client.post(prefix + 'a', 'body');
+
+    await server.shutdown();
+    await subs.cancel();
+  }
+
+  test('log input body', () async {
+    await testBody(true, true);
+  });
+
+  test('no log input body', () async {
+    await testBody(true, false);
+  });
+
+  test('log output body', () async {
+    await testBody(false, true);
+  });
+
+  test('no log output body', () async {
+    await testBody(false, false);
   });
 }
