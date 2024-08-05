cfaa6dc236d89b7b41bc1cbe3c6c8260ee9d2ac8
Alexander Yemelyanov <alex@justprodev.com>
2024-08-06 00:25:27 +0500

    client: api breaking changes:
    - `headers` moved directly to the client, because they are usable for other than auth cases (like inserting accepted language, etc)
    - `onExpire` moved directly to the client just to simplicity

--- lib/src/json_http_client.dart
+++ lib/src/json_http_client.dart
@@ -8,7 +8,6 @@ import 'dart:io' show HttpHeaders, HttpException;
 import 'package:http/http.dart' as http;
 import 'package:json_fetcher/src/util/http.dart';
 
-import 'auth_info.dart';
 import 'http_cache.dart';
 import 'json_fetcher_exception.dart';
 import 'json_http_fetcher.dart';
@@ -28,20 +27,35 @@ import 'json_http_fetcher.dart';
 class JsonHttpClient {
   /// delegated [http.Client] which will be used for all requests
   final http.Client _client;
-  final AuthInfo? auth;
 
-  // register all errors, include parse errors in a fetchers
+  /// cache manager used by [JsonHttpFetcher]
+  /// can be used to directly control the cache (i.e. [HttpCache.emptyCache]/[HttpCache.evict])
+  final HttpCache cache;
+
+  // to catch all errors
   final Function(Object error, StackTrace? trace)? onError;
 
   /// called when new document came from network and parsed
   final Function(String url, Object document)? onFetched;
 
-  /// cache manager used by [JsonHttpFetcher]
-  final HttpCache _cache;
+  /// These headers will be applied to all requests
+  ///
+  /// [url] requested url
+  final Map<String, String> Function(String url)? globalHeaders;
+
+  /// Will be called for 401 error ([authHeaders] will be removed from the header immediately),
+  ///
+  /// [logout]==true means that handler was called  at second, because a new auth data is wrong,
+  ///  and We probably should redirect to login page
+  ///
+  /// MUST return true if token is refreshed successfully
+  final Future<bool> Function(bool logout)? onExpire;
 
   JsonHttpClient(
-    this._client, this._cache, {
-    this.auth,
+    this._client,
+    this.cache, {
+    this.globalHeaders,
+    this.onExpire,
     this.onError,
     this.onFetched,
   });
@@ -70,10 +84,8 @@ class JsonHttpClient {
       _send('POST', url, null,
           headers: headers, skipCheckingExpiration: skipCheckingExpiration, files: files, fields: fields);
 
-  void logout() => auth?.onExpire.call(true);
-
-  /// can be used to directly control the cache (i.e. [HttpCache.emptyCache]/[HttpCache.evict])
-  HttpCache get cache => _cache;
+  /// initiate logout
+  void logout() => onExpire?.call(true);
 
   /// So, basically is just wrapper over [BaseClientExt.sendUnstreamed] which:
   ///
@@ -97,13 +109,11 @@ class JsonHttpClient {
         if (fields != null) request.fields.addAll(fields);
         request.headers.addAll(headers ?? {});
         request.files.addAll(files);
-        final authHeaders = auth?.headers(url);
-        if (authHeaders != null) request.headers.addAll(authHeaders);
+        if (globalHeaders != null) request.headers.addAll(globalHeaders!(url));
         return await _client.send(request).then(http.Response.fromStream);
       } else {
         final Map<String, String> h = {"Content-Type": "application/json"};
-        final authHeaders = auth?.headers(url);
-        if (authHeaders != null) h.addAll(authHeaders);
+        if (globalHeaders != null) h.addAll(globalHeaders!(url));
         if (headers != null) h.addAll(headers);
         return _client.sendUnstreamed(method, Uri.parse(url), h, json);
       }
@@ -119,13 +129,13 @@ class JsonHttpClient {
         }
         if (response.statusCode < 200 || response.statusCode >= 400) {
           /// for 401 error we silently invoke onExpire handler
-          if (response.statusCode == 401 && auth != null && !skipCheckingExpiration) {
+          if (response.statusCode == 401 && onExpire != null && !skipCheckingExpiration) {
             final refreshed = await _onExpire(); // refresh auth data
             if (refreshed) {
               return await action(); // resubmit latest call & return here because error handled inside action (thrown, etc)
             } else {
               // logout
-              await auth!.onExpire.call(true);
+              await onExpire!(true);
               throw HttpException('401', uri: response.request?.url);
             }
           } else {
@@ -150,8 +160,7 @@ class JsonHttpClient {
   /// process onExpire calls in one place
   /// to make additional routing, to prevent extra calls and etc
   Future<bool> _onExpire() {
-    // any case
-    if (auth == null) return Future.value(false);
+    if (onExpire == null) return Future.value(false);
 
     // when we already waiting onExpire() call, then don't create new one,
     // instead we will return the same call
@@ -159,7 +168,7 @@ class JsonHttpClient {
     if (oldCall != null) return oldCall;
 
     // create only if not exists
-    final newCall = auth!.onExpire.call(false);
+    final newCall = onExpire!.call(false);
     // inform everyone that we are ready for a new call, when old is complete
     newCall.whenComplete(() => _onExpireFuture = null);
     _onExpireFuture = newCall;
--- test/utils/create_client.dart
+++ test/utils/create_client.dart
@@ -2,7 +2,6 @@
 
 import 'package:http/http.dart';
 import 'package:json_fetcher/loggable_http_client.dart';
-import 'package:json_fetcher/src/auth_info.dart';
 import 'package:json_fetcher/src/cache/http_cache_io_impl.dart' as io_cache_impl;
 import 'package:json_fetcher/src/http_cache.dart';
 import 'package:json_fetcher/src/json_http_client.dart';
@@ -21,10 +20,11 @@ Future<JsonHttpClient> createClient({
   final JsonHttpClient client = JsonHttpClient(
     LoggableHttpClient(Client(), Logger((JsonHttpClient).toString()), config: config),
     cache ?? io_cache_impl.createCache('temp'),
-    auth: AuthInfo((_) => selectedAuthHeaders, (_) async {
+    globalHeaders: (_) => selectedAuthHeaders,
+    onExpire: (_) async {
       selectedAuthHeaders = authHeaders2;
       return true;
-    }),
+    },
     onFetched: onFetched,
     onError: (e, t) => Logger.root.severe('Error in JsonHttpClient: $e', e, t),
   );
