bec0fdfe4cf2b80e446a58c0311a09e6488dc9ca
Alexander Yemelyanov <alex@justprodev.com>
2024-11-07 21:08:44 +0500

    fix - flutter example - awaiting in not needed while creating the client

--- examples/flutter_json_fetcher_example/lib/api/create_client.dart
+++ examples/flutter_json_fetcher_example/lib/api/create_client.dart
@@ -7,7 +7,7 @@ import 'package:json_fetcher/json_fetcher.dart';
 import 'package:json_fetcher/loggable_http_client.dart';
 import 'package:logging/logging.dart';
 
-FutureOr<JsonHttpClient> createClient([FutureOr<String>? path]) {
+JsonHttpClient createClient([FutureOr<String>? path]) {
   return JsonHttpClient(
     LoggableHttpClient(
       Client(),
