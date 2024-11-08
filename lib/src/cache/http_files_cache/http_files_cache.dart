0f6d583b1c3710bb7ff748ea6ed9bf92e1ca1632
Alexander Yemelyanov <alex@justprodev.com>
2024-11-08 21:48:52 +0500

    `HttpFilesCache`: remove isolate; add concurrent IO

--- lib/src/cache/http_files_cache/http_files_cache.dart
+++ lib/src/cache/http_files_cache/http_files_cache.dart
@@ -1,26 +1,24 @@
 // Created by alex@justprodev.com on 05.08.2024.
 
 import 'dart:async';
+import 'dart:collection';
 import 'dart:io';
 
 import 'package:json_fetcher/src/http_cache.dart';
 import 'package:json_fetcher/src/util/fnv1a_hash/fnv1a_hash.dart';
 import 'package:meta/meta.dart';
 
-import 'http_files_cache_worker.dart';
-
 @visibleForTesting
 const subDirPath = '__http_files_cache';
 
-// one worker for all instances!
-late final HttpFilesCacheWorker _worker;
-bool _workerCreated = false;
+const maxConcurrentJobs = 100;
 
-/// An file-based cache
+/// File-based cache
 ///
-/// 1. Each value is stored in a separate file
-/// 2. Using [HttpFilesCacheWorker] to perform IO operations
-/// 3. Keys should be numbers, see [createKey]
+/// 1. Keys should be numbers, see [createKey]
+/// 2. Each value is stored in a separate file
+/// 3. Running IO operations concurrently, but not more than [maxConcurrentJobs].
+///    Locking by key: Wait for jobs with same key.
 class HttpFilesCache extends HttpCache {
   Completer? _initializing = Completer();
 
@@ -32,12 +30,7 @@ class HttpFilesCache extends HttpCache {
       try {
         /// Using subdirectory to avoid conflicts with other files
         _path = '${await path}/$subDirPath';
-        if (!Directory(_path).existsSync()) await Directory(_path).create(recursive: true);
-
-        if (!_workerCreated) {
-          _worker = await HttpFilesCacheWorker.create();
-          _workerCreated = true;
-        }
+        if (!await Directory(_path).exists()) await Directory(_path).create(recursive: true);
 
         _initializing?.complete();
         _initializing = null;
@@ -52,28 +45,82 @@ class HttpFilesCache extends HttpCache {
   @override
   Future<void> delete(String key) async {
     if (_initializing != null) await _initializing!.future;
-    await _worker.run(DeleteJob(_path, key));
+
+    return _runJob(key, () async {
+      try {
+        await _getFile(key).delete();
+      } on PathNotFoundException {
+        // ignore
+      }
+    });
   }
 
   @override
   Future<String?> get(String key) async {
     if (_initializing != null) await _initializing!.future;
-    return await _worker.run(GetJob(_path, key));
+
+    return _runJob(key, () async {
+      try {
+        return await _getFile(key).readAsString();
+      } on PathNotFoundException {
+        return null;
+      }
+    });
   }
 
   @override
   Future<void> put(String key, String value) async {
     if (_initializing != null) await _initializing!.future;
-    await _worker.run(PutJob(_path, key, value));
+
+    return _runJob(key, () async {
+      final file = _getFile(key);
+      try {
+        await file.writeAsString(value, flush: true);
+      } on PathNotFoundException {
+        await file.parent.create(recursive: true).then((_) => file.writeAsString(value, flush: true));
+      }
+    });
   }
 
   @override
   Future<void> emptyCache() async {
     if (_initializing != null) await _initializing!.future;
-    await _worker.run(EmptyCacheJob(_path));
+
+    final dir = Directory(_path);
+    final tmp = await dir.rename('${dir.path}.${DateTime.now().microsecondsSinceEpoch}');
+    await dir.create();
+    // don't wait for the old cache to be deleted
+    tmp.delete(recursive: true);
   }
 
   /// This implementation uses FNV-1a hash function of the url + body
   @override
   String createKey(String url, {String? body}) => fastHash('$url${body ?? ''}');
+
+  @pragma('vm:prefer-inline')
+  File _getFile(String key) {
+    final integerKey = int.parse(key);
+    final dirPath = '$_path/d_${integerKey ~/ 100}';
+    return File('$dirPath/$key');
+  }
+
+  final _jobs = HashMap<String, Future>();
+
+  Future<T> _runJob<T>(String key, Future<T> Function() job) async {
+    final Future? jobToWait = _jobs.length >= maxConcurrentJobs ? _jobs.values.first : _jobs[key];
+
+    if (jobToWait != null) {
+      await jobToWait;
+      return await _runJob(key, job);
+    }
+
+    final newJob = job();
+
+    try {
+      _jobs[key] = newJob;
+      return await newJob;
+    } finally {
+      _jobs.remove(key);
+    }
+  }
 }
--- test/cache_test.dart
+++ test/cache_test.dart
@@ -9,7 +9,6 @@ import 'package:json_fetcher/json_fetcher.dart' as general show createCache;
 import 'package:json_fetcher/src/cache/http_cache_io_impl.dart' as io_cache_impl;
 import 'package:json_fetcher/src/cache/http_cache_web_impl.dart' as web_cache_impl;
 import 'package:json_fetcher/src/cache/http_files_cache/http_files_cache.dart';
-import 'package:json_fetcher/src/cache/http_files_cache/http_files_cache_worker.dart';
 import 'package:json_fetcher/src/cache/http_hive_cache/http_hive_cache.dart';
 import 'package:json_fetcher/src/http_cache.dart';
 import 'package:json_fetcher/src/util/fnv1a_hash/fnv1a_hash.dart';
@@ -19,12 +18,6 @@ const temp = 'temp';
 
 void main() {
   group('io', () {
-    group('worker', () {
-      test('isolate', () => testWorkerIsolate());
-      test('handleJob', () => testWorkerHandleJob());
-      test('error', () => testWorkerErrors());
-    });
-
     test('cache', () async {
       final cache = io_cache_impl.createCache(temp);
       await testCache(cache);
@@ -88,6 +81,7 @@ Future<void> testCache(HttpCache impl) async {
   expect(await impl.get('123'), null);
   await impl.put('123', 'value3');
   expect(await impl.get('123'), 'value3');
+  await impl.delete('0'); // not exists
 
   // emptyCache
   final keys = ['123', '124', '125'];
@@ -108,64 +102,6 @@ Future<void> testCache(HttpCache impl) async {
   expect(await impl.get(urlKey), null);
 }
 
-Future<void> testWorkerHandleJob() async {
-  final root = Directory('$temp/handle_job');
-  if (root.existsSync()) root.deleteSync(recursive: true);
-  root.createSync(recursive: true);
-
-  final key = '123';
-  final value = 'value';
-  final file = getFile(root.path, key);
-  String? result;
-
-  // put
-  result = HttpFilesCacheWorker.handleJob(PutJob(root.path, key, value));
-  expect(result, null);
-  expect(file.existsSync(), true);
-  expect(file.readAsStringSync(), value);
-
-  // get
-  result = HttpFilesCacheWorker.handleJob(GetJob(root.path, key));
-  expect(result, value);
-
-  // delete
-  result = HttpFilesCacheWorker.handleJob(DeleteJob(root.path, key));
-  expect(result, null);
-  expect(file.existsSync(), false);
-  result = HttpFilesCacheWorker.handleJob(GetJob(root.path, key));
-  expect(result, null);
-
-  // emptyCache
-  HttpFilesCacheWorker.handleJob(PutJob(root.path, key, value));
-  result = HttpFilesCacheWorker.handleJob(EmptyCacheJob(root.path));
-  expect(result, null);
-  expect(file.parent.existsSync(), false);
-  result = HttpFilesCacheWorker.handleJob(GetJob(root.path, key));
-  expect(result, null);
-}
-
-Future<void> testWorkerErrors() async {
-  final worker = await HttpFilesCacheWorker.create();
-  Object? error;
-  final dir = Directory('$temp/nonExistentDir234324324234');
-  if (dir.existsSync()) dir.deleteSync(recursive: true);
-  final job = PutJob(dir.path, '123', 'value');
-  try {
-    await worker.run(job);
-  } catch (e) {
-    error = e;
-  }
-  expect(error, isNotNull);
-  error = null;
-  dir.createSync(recursive: true);
-  try {
-    await worker.run(job);
-  } catch (e) {
-    error = e;
-  }
-  expect(error, isNull);
-}
-
 Future<void> testCreateCacheWithPathByFuture(HttpCache Function(Future<String> path) createCache) async {
   final Future<String> path = Future.delayed(Duration(milliseconds: 10), () => temp);
   final stopwatch = Stopwatch()..start();
@@ -174,32 +110,4 @@ Future<void> testCreateCacheWithPathByFuture(HttpCache Function(Future<String> p
   await cache.put('123', 'value');
   expect(stopwatch.elapsedMilliseconds, greaterThanOrEqualTo(10));
   stopwatch.stop();
-}
-
-Future<void> testWorkerIsolate() async {
-  final path = '$temp/isolate';
-  final dir = Directory(path);
-  if (dir.existsSync()) dir.deleteSync(recursive: true);
-  dir.createSync(recursive: true);
-  final worker = await HttpFilesCacheWorker.create();
-
-  // test concurrency for different keys
-  final numbersFrom0To10 = List.generate(10, (index) => index.toString());
-  await Future.wait(numbersFrom0To10.map((number) => worker.run(PutJob(path, number, number))));
-  final values = await Future.wait(numbersFrom0To10.map((number) => worker.run(GetJob(path, number))));
-  expect(values, numbersFrom0To10);
-
-  // test concurrency for same key
-  final key = numbersFrom0To10.first;
-  await Future.wait(numbersFrom0To10.map((number) => worker.run(PutJob(path, key, number))));
-  final value = await worker.run(GetJob(path, key));
-  expect(value, numbersFrom0To10.last);
-
-  // create exception
-  final message1 = 'isolate not spawned';
-  late String message2;
-  await HttpFilesCacheWorker.create((entryPoint, sendPort, {String? debugName}) async {
-    throw message1;
-  }).then((_) => message2 = 'isolate spawned').catchError((e) => message2 = e);
-  expect(message2, message1);
 }
