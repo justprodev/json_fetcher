42843a955d8b8987e41a84746aa53a3d3ab3ddd4
Alexander Yemelyanov <alex@justprodev.com>
2024-11-10 09:49:51 +0500

    shorter import

--- lib/src/cache/http_cache_io_impl.dart
+++ lib/src/cache/http_cache_io_impl.dart
@@ -2,9 +2,8 @@
 
 import 'dart:async';
 
-import 'package:json_fetcher/src/cache/http_files_cache/http_files_cache.dart';
-
 import '../http_cache.dart';
+import 'http_files_cache/http_files_cache.dart';
 
 HttpCache createCache([FutureOr<String>? path]) {
   assert(path != null, 'Path must be provided for IO cache');
