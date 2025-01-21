dadf78392b961255dac18cf6d42306ee135a4d88
Емельянов Александр <a.yemelyanov@gmail.com>
2025-01-21 14:41:33 +0500

    revert typos

--- benchmarks/dart_cache_benchmark/bin/dart_cache_benchmark.dart
+++ benchmarks/dart_cache_benchmark/bin/dart_cache_benchmark.dart
@@ -2,12 +2,11 @@ import 'dart:io';
 
 import 'package:dart_cache_benchmark/benchmark.dart';
 import 'package:dart_cache_benchmark/http_hive_cache.dart';
-
 // ignore_for_file: implementation_imports
-import 'package:json_fetcher/src/cache/http_web_cache/http_web_cache.dart';
+import 'package:json_fetcher/src/cache/http_files_cache/http_files_cache.dart';
 
 void main() async {
-  await for (final s in runBenchmark([HttpHiveCache(null), HttpWebCache()])) {
+  await for (final s in runBenchmark([HttpHiveCache('temp'), HttpFilesCache('temp')])) {
     stdout.write(s);
   }
 }
